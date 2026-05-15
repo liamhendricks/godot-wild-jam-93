1a1f42c59c0b01482a984130bcd28de6074266e0
braydenbrockbank <braydenbrockbank@icloud.com>
2026-05-14 22:34:24 -0600

    timer and mothership

--- godot-wild-jam-93/main.gd
+++ godot-wild-jam-93/main.gd
@@ -4,14 +4,32 @@ extends Node2D
 @onready var shape_generator : ShapeGenerator = $ShapeGenerator
 @onready var player : CharacterBody2D = $Player
 @onready var state_machine : StateMachine = $StateMachine
+var enter_timer = 10.0
+var exit_timer = 20
+var mother_ship = preload("res://entities/MotherShip.tscn")
+var mother_arriving = null
 
 func _ready() -> void:
 	state_machine.init(player)
 	UserInterface.init(player, shape_generator)
-
+	
+	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	camera.position = player.position
+	enter_timer = enter_timer - delta
+	exit_timer = exit_timer - delta
+	if enter_timer <= 0:
+		mother_arriving = mother_ship.instantiate()
+		mother_arriving.position = player.position
+		add_child(mother_arriving)#mother is here 
+		enter_timer = 20.0
+		exit_timer = 10
+	if exit_timer <= 0:
+		if mother_arriving != null:
+			mother_arriving.queue_free()
+			mother_arriving = null
+		
